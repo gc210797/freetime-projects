788f62452e763c9741731a037b737163ac0d8e80
gc210797 <gc210797@gmail.com>
2018-04-12 19:11:11 +0530

    added missing function headers

--- space warrior/engine.h
+++ space warrior/engine.h
@@ -9,11 +9,12 @@ int engine_init(const char *title, int win_w, int win_h);
 void engine_quit();
 void *player_new(int health, const char *t_path);
 void player_del(PLAYER ref);
-int player_dec_health(PLAYER ref);
+int player_dec_health(PLAYER ref, PLAYER obj);
 int player_get_health(PLAYER ref);
 void render_beg();
 void render_end();
 void player_render(PLAYER ref);
 void player_set_pos(PLAYER ref, int x, int y);
+int player_check_collision(PLAYER play, PLAYER obj);
 
 #endif
