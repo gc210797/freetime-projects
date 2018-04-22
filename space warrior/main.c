319c2212567ab8cefa4e5e116e22bfd7b118a70e
gc210797 <gc210797@gmail.com>
2018-04-22 20:49:48 +0530

    Added score text, health text and fixed invalid address read error(reported in valgrind). Also fixed score reduction when enemy goes out of screen

--- space warrior/main.c
+++ space warrior/main.c
@@ -62,6 +62,14 @@ struct enemies *shoot_collision(struct enemies *shoot, struct enemies *ae)
 	return NULL;
 }
 
+int math_mod(int val)
+{
+	if(val < 0)
+		return -val;
+	else
+		return val;
+}
+
 int main()
 {
 	engine_init("Space invader", 640, 480);
@@ -74,6 +82,14 @@ int main()
 	struct enemies *it, *it2; //used for iteration
 	PLAYER play;
 	int pp_x, pp_y, pvel;
+	Uint32 new_em;
+	unsigned long long score;
+	TEXT score_disp;
+	char score_disp_str[12];
+	int em_pos_cache;
+	int health;
+	TEXT health_disp;
+	char health_disp_str[12];
 
 	ae = NULL;
 	shoot = NULL;
@@ -83,8 +99,15 @@ int main()
 	pp_x = 640 / 2; //half of window width
 	pp_y = 480 - 32; //height - height of image
 	pvel = 0;
-	for(i = 0; i < 15; i++)
-		enemies_add(&ae, 0, i * 42, 0, NULL);
+	score = 0;
+	em_pos_cache = -1;
+	score_disp = text_new("Score: 0", 640 - 100, 5, 100, 30, 0xff, 0xff, 0xff,
+			"arial.ttf", 25);
+	health_disp = text_new("Health: 25", 0, 5, 100, 30, 0xff, 0xff, 0xff,
+			"arial.ttf", 25);
+	for(i = 0; i < 5; i++)
+		enemies_add(&ae, 0, rand() % 598, 0, NULL); //598: width - width of image
+	new_em = SDL_GetTicks();
 
 	while(!quit && play) { //TODO: Restructure it.........
 		while(SDL_PollEvent(&e)) {
@@ -116,6 +139,21 @@ int main()
 			}
 		}
 
+		if((SDL_GetTicks() - new_em) / 1000 >= 2) {
+			for(i = 0; i < 3; i++) {
+				int rnd = rand() % 598;
+				if(math_mod(rnd - em_pos_cache) <= 42) {
+					if(rnd - em_pos_cache >= 0)
+						rnd += 44;
+					else
+						rnd -= 44;
+				}
+				enemies_add(&ae, 0, rnd, 0, NULL);
+				em_pos_cache = rnd;
+			}
+			new_em = SDL_GetTicks();
+		}
+
 		pp_x += pvel;
 
 		player_set_pos(play, pp_x, pp_y);
@@ -127,39 +165,69 @@ int main()
 		for(it = shoot; it != NULL; it = it->next)
 			player_render(it->e);
 
-		for(it = shoot; it != NULL; it = it->next) {
+		for(it = shoot; it != NULL; ) {
+			struct enemies *rm;
 			if((it2 = shoot_collision(it, ae)) != NULL) {
 				player_dec_health(it2->e, it->e);
-				enemies_rm(&shoot, it);
+				rm = it;
+				it = it->next;
+				enemies_rm(&shoot, rm);
 			}
 			else if(it->py <= 0) {
-				enemies_rm(&shoot, it);
+				rm = it;
+				it = it->next;
+				enemies_rm(&shoot, rm);
 			}
 			else {
 				it->py -= 5;
 				player_set_pos(it->e, it->px, it->py);
+				it = it->next;
 			}
 		}
 
-		for(it = ae; it != NULL; it = it->next) {
+		for(it = ae; it != NULL; ) {
+			struct enemies *rm;
 			if(player_check_collision(it->e, play)) {
 				player_dec_health(play, it->e);
-				enemies_rm(&ae, it);
+				rm = it;
+				it = it->next;
+				enemies_rm(&ae, rm);
 			}
-			else if(it->py <= -480) {
-				enemies_rm(&ae, it);
+			else if(it->py >= 480) {
+				rm = it;
+				it = it->next;
+				enemies_rm(&ae, rm);
+				if(score != 0)
+					score -= 1;
 			}
 			else {
 				it->py += 1;
 				player_set_pos(it->e, it->px, it->py);
+				it = it->next;
 			}
 		}
 
-		for(it = ae; it != NULL; it = it->next) {
-			if(player_get_health(it->e) <= 0)
-				enemies_rm(&ae, it);
+		for(it = ae; it != NULL;) {
+			struct enemies *rm;
+			if(player_get_health(it->e) <= 0) {
+				rm = it;
+				it = it->next;
+				enemies_rm(&ae, rm);
+				score++;
+			}
+			else {
+				it = it->next;
+			}
 		}
 
+		sprintf(score_disp_str, "Score: %lld", score);
+		text_mod_str(score_disp, score_disp_str);
+		text_render(score_disp);
+		
+		sprintf(health_disp_str, "Health: %d", player_get_health(play));
+		text_mod_str(health_disp, health_disp_str);
+		text_render(health_disp);
+		
 		render_end();
 
 		if(player_get_health(play) <= 0) {
@@ -168,6 +236,9 @@ int main()
 		}
 	}
 
+	if(play == NULL)
+		printf("Score: %ld\n", score);
+
 	while(ae != NULL)
 		enemies_rm(&ae, ae);
 	while(shoot != NULL)
@@ -175,6 +246,10 @@ int main()
 
 	if(play != NULL)
 		player_del(play);
+	if(score_disp != NULL)
+		text_rm(score_disp);
+	if(health_disp != NULL)
+		text_rm(health_disp);
 
 	engine_quit();
 }
