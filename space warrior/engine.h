7fb4a67c64ec279d9247522df6627dbec3495ce2
gc210797 <gc210797@gmail.com>
2018-04-22 20:48:38 +0530

    added text texture support function header

--- space warrior/engine.h
+++ space warrior/engine.h
@@ -3,7 +3,8 @@
 
 #include <SDL2/SDL.h>
 
-typedef void * PLAYER;
+typedef void * PLAYER; //don't ever do this!!!!!!!!
+typedef struct text * TEXT; //better
 
 int engine_init(const char *title, int win_w, int win_h);
 void engine_quit();
@@ -11,6 +12,12 @@ void *player_new(int health, const char *t_path);
 void player_del(PLAYER ref);
 int player_dec_health(PLAYER ref, PLAYER obj);
 int player_get_health(PLAYER ref);
+TEXT text_new(const char *str, int x, int y, int w, int h,
+	Uint8 r, Uint8 g, Uint8 b, const char *font, int size);
+int text_mod_str(TEXT, const char *str);
+void text_mod_pos(TEXT, int x, int y, int w, int h);
+void text_render(TEXT);
+void text_rm(TEXT t);
 void render_beg();
 void render_end();
 void player_render(PLAYER ref);
